6f1d69609a90dd5dfc5be29a69323fffdc570cf9
https://github.com/CristianJl2001/2670586-ADSO.git <https://github.com/CristianJl2001/2670586-ADSO.git>
2024-04-04 17:22:48 -0500

    hola mundo

---
+++ appFarmacias/src/principal/ButtonEditar.java
@@ -0,0 +1,16 @@
+/
+package principal;
+
+import javax.swing.JCheckBox;
+import javax.swing.table.TableCellEditor;
+
+/**
+ *
+ * @author Cristian
+ */
+public class ButtonEditar implements TableCellEditor {
+
+    public ButtonEditar(JCheckBox jCheckBox) {
+    }
+    
+}
---
+++ appFarmacias/src/principal/stockProducto.java
@@ -0,0 +1,146 @@
+
+package principal;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JCheckBox;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import utils.BaseDatos;
+import utils.ButtonEditar;
+import utils.ButtonEditor;
+import utils.ButtonRenderer;
+
+
+public class stockProducto extends javax.swing.JPanel {
+    BaseDatos bd;
+    DefaultTableModel model = new DefaultTableModel();
+    JTable tablaDatos = new javax.swing.JTable();
+   
+    public stockProducto(BaseDatos bd) {
+        this.bd = bd; 
+        //imprimirProveedor();
+        initComponents();
+    }
+    public void initAlternComponents(){
+        setLocationRelativeTo(null);
+        setVisible(true);
+        model = (DefaultTableModel) tablaDatos.getModel();
+        
+        model = (DefaultTableModel) tablaDatos.getModel();
+        
+        // Tamaño de Columnas
+        tablaDatos.getColumnModel().getColumn(9).setCellEditor(new DefaultCellEditor(new JCheckBox()));
+        tablaDatos.getColumnModel().getColumn(9).setCellRenderer(new ButtonRenderer());
+        
+        tablaDatos.getColumnModel().getColumn(10).setCellEditor(new ButtonEditor(new JCheckBox()));
+        tablaDatos.getColumnModel().getColumn(10).setCellRenderer(new ButtonRenderer());
+        
+        // Tamaño de Columnas
+        tablaDatos.getColumnModel().getColumn(0).setPreferredWidth(50);
+        tablaDatos.getColumnModel().getColumn(1).setPreferredWidth(150);
+        tablaDatos.getColumnModel().getColumn(2).setPreferredWidth(150);
+        tablaDatos.getColumnModel().getColumn(3).setPreferredWidth(50);
+        tablaDatos.getColumnModel().getColumn(4).setPreferredWidth(150);
+        tablaDatos.getColumnModel().getColumn(5).setPreferredWidth(10);
+        tablaDatos.getColumnModel().getColumn(6).setPreferredWidth(10);
+        tablaDatos.getColumnModel().getColumn(7).setPreferredWidth(10);
+        tablaDatos.getColumnModel().getColumn(8).setPreferredWidth(10);
+        
+        // Ajuste del Orden y Ancho de Columnas
+        tablaDatos.getTableHeader().setReorderingAllowed(false);
+        tablaDatos.getTableHeader().setResizingAllowed(false);
+        
+        // Centrar contenido de columnas
+        DefaultTableCellRenderer centerRender = new DefaultTableCellRenderer();
+        centerRender.setHorizontalAlignment(SwingConstants.CENTER);
+        tablaDatos.getColumnModel().getColumn(0).setCellRenderer(centerRender);
+        tablaDatos.getColumnModel().getColumn(3).setCellRenderer(centerRender);
+        tablaDatos.getColumnModel().getColumn(4).setCellRenderer(centerRender);
+        
+        // Ajuste del Orden y Ancho de Columnas
+        tablaDatos.getTableHeader().setReorderingAllowed(false);
+        tablaDatos.getTableHeader().setResizingAllowed(false);
+        
+        
+         // Alto de las filas
+        tablaDatos.setRowHeight(20);
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tableStock = new javax.swing.JTable();
+        etq_titulo = new javax.swing.JLabel();
+
+        tableStock.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "id_stock", "NIT_farmacia", "id_producto", "proveedor", "cant_entrante", "cant_restante", "estado", "fecha_registro", "fecha_descontinuacion", "", ""
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class, java.lang.Object.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false, true, true, true, true
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tableStock);
+
+        etq_titulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        etq_titulo.setText("STOCK");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(244, 244, 244)
+                        .addComponent(etq_titulo, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(32, 32, 32)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 715, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(243, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(etq_titulo)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(38, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel etq_titulo;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tableStock;
+    // End of variables declaration//GEN-END:variables
+
+    private void setLocationRelativeTo(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
